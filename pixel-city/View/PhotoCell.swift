3ca94a01097dbe1ca0993852037d969dbec99f80
MinRiDaddy <lovelyroom77@gmail.com>
2018-07-07 15:12:36 +0900

    7월 7일 commit

---
+++ pixel-city/View/PhotoCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoCell.swift
+//  pixel-city
+//
+//  Created by MinRiDaddy on 2018. 7. 7..
+//  Copyright © 2018년 MinRiDaddy. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
